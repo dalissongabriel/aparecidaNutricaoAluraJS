5906f774c058e95d694413ac6e7511a2d24fb1f2
Alisson Gabriel <alissongabrieldino@gmail.com>
2020-09-14 08:43:24 -0300

    Refatorar: Melhorar mensagem de erro durante o fech a api

--- js/consumirPacientesAPI.js
+++ js/consumirPacientesAPI.js
@@ -3,7 +3,7 @@ var btnBuscaPacientes = document.querySelector('#buscar-pacientes')
 btnBuscaPacientes.addEventListener('click',()=>buscarPacientesNaApi())
 
 async function buscarPacientesNaApi() {
-    let api = 'https://api-pacientes.herokuapp.com/pacientes'
+    let api = 'https://api-pacientes.herokuapp.com/paciente'
     let response = await fetch(api)
     if (response.ok) {
         let pacientes = await response.json()
@@ -11,6 +11,10 @@ async function buscarPacientesNaApi() {
             inserePacientesNaTabela(paciente)
         })
     } else {
-        alert("Erro ao tentar importar os pacientes")
+        
+        alert(`Erro ao tentar importar os pacientes.
+        Código do erro: ${response.status}
+        Entre em contato com o suporte!`
+        )
     }
 }
